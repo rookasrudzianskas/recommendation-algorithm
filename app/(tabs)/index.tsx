3c0d32bfd8002648a5d5b7692a83c7d8efdc5d4c
Rokas <rookas.rudzenskas@gmail.com>
2023-10-10 16:33:25 +0300

    initial ommit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import {View, Text} from 'react-native';
+import {StatusBar} from "expo-status-bar";
+export default function TabOneScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
