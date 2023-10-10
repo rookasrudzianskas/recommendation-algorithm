b4cf872b51ee6e603fb7d1b1fce111f2558c0455
Rokas <rookas.rudzenskas@gmail.com>
2023-10-10 17:34:29 +0300

    fix

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import {StatusBar} from "expo-status-bar";
 export default function TabOneScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Connect to Supabase</Text>
       <StatusBar style="auto" />
     </View>
   );
