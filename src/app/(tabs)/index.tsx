767545186d87ea5a8d2f43a6afc82a8f74bef69c
Rokas <rookas.rudzenskas@gmail.com>
2023-10-12 10:01:56 +0300

    fix

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,23 @@
 import {View, Text} from 'react-native';
 import {StatusBar} from "expo-status-bar";
+import {useEffect, useState} from "react";
+import {supabase} from "../../lib/supabase";
 export default function TabOneScreen() {
+  const [movies, setMovies] = useState<any>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const { data: movies } = await supabase
+        .from("movies")
+        .select("*")
+        // .range(0, 25);
+      setMovies(movies);
+    };
+    fetchMovies();
+  }, []);
+
+  console.log(JSON.stringify(movies));
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text>Connect to Supabase</Text>
